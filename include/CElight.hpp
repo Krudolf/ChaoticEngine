677ca17fc14eb7afff9e8e93f0251b3ea961943d
Manu <manuelquiranteartiaga@gmail.com>
2018-02-13 15:42:59 +0100

    Luces bien
    Cambio en las propiedades de las luces

--- include/CElight.hpp
+++ include/CElight.hpp
@@ -6,18 +6,17 @@
 
 class CELight : public CEEntity{
 public:
-	CELight(glm::vec3 m_position, glm::vec3 m_intensities);
+	CELight(glm::vec3 m_intensities, float m_attenuation);
 	~CELight();
 
 	void beginDraw();
 	void endDraw();
-	void setPosition(glm::vec3 p_position);
 	void setIntensities(glm::vec3 p_intensities);
+	void setAtenuation(float p_attenuation);
 
 private:
-	glm::vec3 m_position;
     glm::vec3 m_intensities; //(0-1)red, green and blue intensities 
-    float attenuation;
+    float m_attenuation;
 };
 
 #endif
--- src/CElight.cpp
+++ src/CElight.cpp
@@ -1,18 +1,15 @@
 #include <iostream>
 #include <../include/CElight.hpp>
 
-CELight::CELight(glm::vec3 p_position, glm::vec3 p_intensities) : CEEntity(){
-	m_position = p_position;
+CELight::CELight(glm::vec3 p_intensities, float p_attenuation) : CEEntity(){
 	m_intensities = p_intensities;
+	m_attenuation = p_attenuation;
 }
 
 CELight::~CELight(){}
 
 void CELight::beginDraw(){
-	std::cout<<"Dibujamos luz en la posicion" <<", "<< m_position.x <<", "<< m_position.y <<", "<< m_position.z << std::endl;
 	std::cout<<"Intensidad de la luz" <<", "<< m_intensities.x <<", "<< m_intensities.y <<", "<< m_intensities.z << std::endl;
-	setPosition(glm::vec3(200, 5, 89));
-	std::cout<<"Dibujamos luz en la posicion" <<", "<< m_position.x <<", "<< m_position.y <<", "<< m_position.z << std::endl;
 }
 
 void CELight::endDraw(){}
@@ -23,8 +20,6 @@ void CELight::setIntensities(glm::vec3 p_intensities){
 	m_intensities.z = p_intensities.z;
 }
 
-void CELight::setPosition(glm::vec3 p_position){
-	m_position.x = p_position.x;
-	m_position.y = p_position.y;
-	m_position.z = p_position.z;
+void CELight::setAtenuation(float p_attenuation){
+	m_attenuation = p_attenuation;
 }
